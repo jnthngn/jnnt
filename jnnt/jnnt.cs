ba1cd36a49dc568de17a521a9fb10582a3a7f604
jnthngn <jnthngn@gmail.com>
2018-06-18 00:59:16 -0300

    Testes

---
+++ jnnt/jnnt.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Robocode;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jnnt
+{
+    public class jnnt : Robot
+    {
+        public override void Run()
+        {
+
+                
+            while (true)
+            {
+                Ahead(200);
+                TurnLeft(90);
+                Ahead(200);
+                TurnRight(90);
+                TurnRadarLeft(25);
+                TurnRadarRight(25);
+                         
+                FireBullet(1);
+
+               
+                
+              
+            }
+
+            
+
+
+        }
+    }
+}
